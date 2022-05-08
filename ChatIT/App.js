c6ef6acb213366ccb8cad2139650cc2ce029d3a3
Rotbert <vrobert_1999@yahoo.com>
2022-05-08 13:21:51 +0300

    added ignore for android warning

--- ChatIT/App.js
+++ ChatIT/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./navigation/Navigation";
+import { LogBox } from "react-native";
 
 export default function App() {
+  LogBox.ignoreLogs(["Setting a timer for a long"]);
+
   return (
     <SafeAreaProvider>
       <Navigation />
